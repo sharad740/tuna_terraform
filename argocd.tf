ee032d0465f415c0aec4ecf4cfe029c46f29f1d1
Sharad Bhandari <bsharad740@gmail.com>
2024-07-04 11:04:34 +0000

    puntey commit

--- argocd.tf
+++ argocd.tf
@@ -9,11 +9,11 @@ resource "kubernetes_namespace" "argocd" {
 resource "helm_release" "argocd" {
   name       = "argocd"
   chart      = "argo-cd"
-  cleanup_on_fail = "true"
+  cleanup_on_fail = true
   repository = "https://argoproj.github.io/argo-helm"
   version    = "7.3.4"
   wait       = true
-  wait_for_jobs = true
+  wait_for_jobs = false
   namespace  = kubernetes_namespace.argocd.metadata[0].name
   values = [file("${path.module}/argocd/values.yaml")]  # Use custom values.yaml
 }
--- nginx-controller.tf
+++ nginx-controller.tf
@@ -10,10 +10,10 @@ resource "helm_release" "nginx_ingress" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   version    = "4.7.0"  # Adjust the version as needed
   namespace  = kubernetes_namespace.ingress_nginx.metadata[0].name
-  cleanup_on_fail = "true"
+  cleanup_on_fail = true
   timeout  = 200
-  wait_for_jobs = "true"
-  wait       = true
+  wait_for_jobs = false
+  wait       = false
 
   set {
     name  = "controller.replicaCount"
@@ -21,8 +21,18 @@ resource "helm_release" "nginx_ingress" {
   }
 
   set {
+    name = "controller.publishService.enabled"
+    value = "true"
+  }
+
+  set {
     name  = "controller.service.type"
-    value = "LoadBalancer"
+    value = "NodePort"
+  }
+
+  set {
+    name  = "controller.service.loadBalancerIP"
+    value = "194.195.112.112"
   }
 
   set {
